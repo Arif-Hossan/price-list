c355fdd55e0a05202695e2f9b09f2ff915636579
Arif Hossan <arif.hossan.eu@gmail.com>
2023-09-12 23:44:01 +0600

    navbar structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
+        <Navbar></Navbar>
         <h1 className='text-7xl text-purple-600'>Hello ,Buddy</h1>
       </div>
     </>

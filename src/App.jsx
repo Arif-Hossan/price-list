7157e0ee881e69bacc543cf498fccbbd877df761
Arif Hossan <arif.hossan.eu@gmail.com>
2023-09-12 22:11:15 +0600

    project setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-7xl text-purple-600'>Hello ,Buddy</h1>
+      </div>
+    </>
+  )
+}
+
+export default App

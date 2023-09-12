fa6e2025ec3afa41dee5700ae22e0582bece7c96
Arif Hossan <arif.hossan.eu@gmail.com>
2023-09-13 04:12:32 +0600

    making navbar responsive

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 // import React from 'react';
 
 const Link = ({route}) => {
     return (
-        <li className="mr-12">
-            <a href={route.path}>{route.name}</a>
+        <li className="mr-14 md:inline-block ">
+            <a className="hover:bg-purple-800 md:block  md:p-3 p-1.5 hover:text-white " href={route.path}>{route.name}</a>
         </li>
     );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,50 +2,53 @@
 
 import { useState } from "react";
 import Link from "../Link/Link";
-import { Bars3Icon,XMarkIcon } from '@heroicons/react/24/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
     // menu bar icon state
-    const [isOpen,setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
     const routes = [
         {
-          "id": 1,
-          "name": "Home",
-          "path": "/"
+            "id": 1,
+            "name": "Home",
+            "path": "/"
         },
         {
-          "id": 2,
-          "name": "About",
-          "path": "/about"
+            "id": 2,
+            "name": "About",
+            "path": "/about"
         },
         {
-          "id": 3,
-          "name": "Services",
-          "path": "/services"
+            "id": 3,
+            "name": "Services",
+            "path": "/services"
         },
         {
-          "id": 4,
-          "name": "Contact",
-          "path": "/contact"
+            "id": 4,
+            "name": "Contact",
+            "path": "/contact"
         },
         {
-          "id": 5,
-          "name": "Blog",
-          "path": "/blog"
+            "id": 5,
+            "name": "Blog",
+            "path": "/blog"
         }
-      ];
-      
+    ];
+
     return (
         <nav>
-            <div onClick={()=>setOpen(!isOpen)}>
-                <span>{isOpen? "Open" :"Close"}</span>
-            <Bars3Icon className="h-6 w-6 text-purple-500" />
-            <XMarkIcon className="h-6 w-6 text-purple-500" />
+            <div onClick={() => setOpen(!open)} className="md:hidden">
+                <span>{open ?
+                    <XMarkIcon className="h-6 w-6 text-purple-500" />
+                    : <Bars3Icon className="h-6 w-6 text-purple-500" />
+                }</span>
+
+
             </div>
-            <ul className="md:flex">
-            {
-                routes.map(route => <Link key={route.id} route={route}></Link>)
-            }
+            <ul className={`md:flex w-full text-center md:justify-center absolute md:static bg-purple-500 duration-500 p-1 md:p-0 ${open ? "top-6" : "-top-36"}`}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
             </ul>
         </nav>
     );

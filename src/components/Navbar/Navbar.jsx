1f1f0b622bc16da7d454c9ec1c93918586bf8dba
Arif Hossan <arif.hossan.eu@gmail.com>
2023-09-12 23:54:39 +0600

    heroicon added for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 // import React from 'react';
 
+import { useState } from "react";
 import Link from "../Link/Link";
+import { Bars3Icon,XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    // menu bar icon state
+    const [isOpen,setOpen] = useState(false)
     const routes = [
         {
           "id": 1,
@@ -33,6 +37,11 @@ const Navbar = () => {
       
     return (
         <nav>
+            <div onClick={()=>setOpen(!isOpen)}>
+                <span>{isOpen? "Open" :"Close"}</span>
+            <Bars3Icon className="h-6 w-6 text-purple-500" />
+            <XMarkIcon className="h-6 w-6 text-purple-500" />
+            </div>
             <ul className="md:flex">
             {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
